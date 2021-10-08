e0364f957c11f5b4579ead8faf8a10d234b85630
Atanas Delevski <adelevski@gmail.com>
2021-10-08 14:18:27 -0500

    updated bot

--- ElonMuskTweetSentimentAnalysis/bot.py
+++ ElonMuskTweetSentimentAnalysis/bot.py
@@ -1,40 +1,37 @@
 class Algo(QCAlgorithm):
 
     def Initialize(self):
-        self.SetStartDate(2012, 11, 1)
-        self.SetEndDate(2017, 1, 1)
+        self.SetStartDate(2012, 12, 1)
+        self.SetEndDate(2021, 3, 22)
         self.SetCash(100000)
 
         self.tsla = self.AddEquity("TSLA", Resolution.Minute).Symbol
-        self.musk = self.AddData(Tweet, "MUSKTWTS", Resolution.Minute).Symbol
+        self.musk = self.AddData(MuskTweet, "MUSKTWTS", Resolution.Minute).Symbol
 
         self.Schedule.On(self.DateRules.EveryDay(self.tsla),
-                        self.TimeRules.BeforeMarketClose(self.tsla, 15),
-                        self.ExitPositions)
-                        
-        self.tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
-        self.model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+                         self.TimeRules.BeforeMarketClose(self.tsla, 15),
+                         self.ExitPositions)
 
     def OnData(self, data):
         if self.musk in data:
             score = data[self.musk].Value
             content = data[self.musk].Tweet
 
-            if score >= 4:
+            if score == 5:
                 self.SetHoldings(self.tsla, 1)
-            elif score <= 2:
+            elif score == 1:
                 self.SetHoldings(self.tsla, -1)
             
-            if score <= 2 or score >= 4:
+            if score == 1 or score == 5:
                 self.Log("Score: " + str(score) + ", Tweet: " + content)
     
     def ExitPositions(self):
         self.Liquidate()
 
-class Tweet(PythonData):
+class MuskTweet(PythonData):
 
     def GetSource(self, config, date, isLive):
-        source = "https://www.dropbox.com/s/ovnsrgg1fou1y0r/MuskTweetsPreProcessed.csv?dl=1"
+        source = "https://downgit.github.io/#/home?url=https://github.com/adelevski/QuantConnect/blob/main/ElonMuskTweetSentimentAnalysis/data/(Final)ElonMuskTweets.csv"
         return SubscriptionDataSource(source, SubscriptionTransportMedium.RemoteFile)
 
     def Reader(self, config, line, date, isLive):
@@ -43,23 +40,25 @@ class Tweet(PythonData):
 
         data = line.split(",")
         tweet = MuskTweet()
-        
-        words_of_interest = ["tsla", "tesla", "model", "cybertruck", "supercharger"]
 
         try:
             tweet.Symbol = config.Symbol
             tweet.Time = datetime.strptime(data[0], "%Y-%m-%d %H:%M:%S") + timedelta(minutes=1)
             content = data[1].lower()
-
+            scores = data[2]
+            
+            words_of_interest = ["tsla", "tesla", "model", "cybertruck", "supercharger", "roadster", "battery" "lithium", "motor"]
+            of_interest = False
+            
             for word in words_of_interest:
                 if word in content:
-                    tokens = self.tokenizer.encode(content, return_tensors="pt")
-                    result = self.model(tokens)
-                    tweet.Value = max([(i+1, r) for i,r in enumerate(result.logits[0])], key=lambda x: x[1])[0]
+                    tweet.Value = max([(i,s) for i,s in enumerate(scores)], key=lambda x: x[1])[0]
+                    of_interest = True
                     break
-                else:
-                    tweet.Value = 3
-
+                
+            if not of_interest:
+                tweet.Value = 3
+                
             tweet["Tweet"] = str(content)
         
         except ValueError:
